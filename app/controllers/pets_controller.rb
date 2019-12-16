16cae4a6e90e6ff08e0c2103a93ec72b492cd56d
Chaya Deaver <chayadeaver@gmail.com>
2019-12-16 11:20:36 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,22 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    # if !params[:pet].keys.include?("owner_ids")
+    #   params[:owner]["owner_ids"] = []
+    #   end
 
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(params["pet"])
+    # binding.pry
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect to "/pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -19,8 +29,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+  
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+    # binding.pry
+  end
 
-    redirect to "pets/#{@pet.id}"
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    # binding.pry
+    @pet.update(params["pet"])
+    
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    
+    @pet.save
+    redirect to "/pets/#{@pet.id}"
   end
 end
